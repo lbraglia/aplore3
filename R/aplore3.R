61e2886c6bb0e68bacb3ad6d742748f1d982020d
lbraglia <lbraglia@gmail.com>
2014-11-30 16:23:35 +0100

    ?aplore3

---
+++ R/aplore3.R
@@ -0,0 +1,11 @@
+#' Datasets from Hosmer, Lemeshow, Sturdivant "Applied Logistic Regression" (3rd ed.)
+#'
+#' This package is a 'unofficial' companion to the textbook Applied
+#' Logistic Regression (3rd ed.,2013) by DW. Hosmer, S. Lemeshow and
+#' RX. Sturdivant. It includes all the data sets used in the book, to
+#' make even more quick/simple to reproduce analysis from R.
+#' 
+#' @name aplore3
+#' @docType package
+#' 
+NULL
