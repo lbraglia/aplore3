eebef58b92041908def1906fe0960b690f076887
lbraglia <lbraglia@gmail.com>
2014-12-01 19:03:41 +0100

    better template

--- data-raw/template.r
+++ data-raw/template.r
@@ -4,14 +4,13 @@ dataset <- read.table(file = "DATASET/DATASET.txt",
                       as.is = TRUE,
                       header = TRUE)
 names(dataset) <- tolower(names(dataset))
-## low.label <- c(">= 2500 g","< 2500 g")
-## race.label <- c("White", "Black", "Other")
-## no.one.twoetc <- c("None", "One", "Two, etc.")
+## var.label <- c("foo","bar")
 
 dataset <- within(dataset, {
 
 })
 dataset <- dataset[order(dataset$id), ]
 rownames(dataset) <- NULL
+summary(dataset)
 
 save("dataset", file = "../data/dataset.rda")
