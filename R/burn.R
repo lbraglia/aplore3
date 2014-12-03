693509564563cd71cd77ff3b455c8bc1d29eab12
lbraglia <lbraglia@gmail.com>
2014-12-03 15:14:15 +0100

    improved burn examples

--- R/burn.R
+++ R/burn.R
@@ -17,4 +17,8 @@
 #' @examples
 #' head(burn1000, n = 10)
 #' summary(burn1000)
+#'
+#' ## Table 3.15 p. 80
+#' summary(mod3.15 <- glm(death ~ tbsa + inh_inj + age + gender + flame + race,
+#'                        family = binomial, data = burn1000 ))
 "burn1000"
