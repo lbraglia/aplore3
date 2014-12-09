a652bf4f9758b41482fa1c715b94956a1e4f6634
lbraglia <lbraglia@gmail.com>
2014-12-09 21:24:08 +0100

    scale_example

---
+++ data-raw/scale_example.R
@@ -0,0 +1,16 @@
+
+source("utils.r")
+
+## SCALE_EXAMPLE
+## -------
+scale_example <- read.table(file = "Scale_Example/Scale_Example.txt",
+                    as.is = TRUE,
+                    header = FALSE)
+names(scale_example) <- c("y", "x")
+scale_example <- within(scale_example, {
+  y <- noYes(y)
+})
+dim(scale_example)
+summary(scale_example)
+
+save("scale_example", file = "../data/scale_example.rda")
